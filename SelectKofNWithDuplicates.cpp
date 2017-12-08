93963d271d7be49eeae00de5eb72da468bf0d464
Alex Boyko <19517759+Barrot@users.noreply.github.com>
2017-12-07 20:05:42 -0600

    Initial implementation without testing

--- SelectKofNWithDuplicates.cpp
+++ SelectKofNWithDuplicates.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <array>
 
 using namespace std;
 
 void SelectKofNWithDuplicates(unsigned int index,
-                              vector<unsigned int> selections,
+                              array<unsigned int, 2> selections,
                               vector<char> &dat,
                               vector<vector<char>> &results) {
-  return;
+  if (index == selections.max_size() - 1) {
+    vector<char> result;
+    for (auto i : selections) {
+      result.emplace_back(dat[i]);
+    }
+    results.emplace_back(move(result));
+  } else {
+    unsigned int start = 0;
+    if (index > 0)
+      start = selections[index - 1];
+    for (unsigned int i = start; i < dat.size(); ++i) {
+      selections[index] = i;
+      SelectKofNWithDuplicates(index + 1, selections, dat, reults);
+    }
+  }
 }
 
-int main() { return 0; }
+int main() { 
+
+  vector<char> dat{'A', 'B', 'C'}; // N = 3
+  vector<vector<char>> results;
+  array<unsigned int, 2> selections; // K = 2
+  SelectKofNWithDuplicates(0, selections, dat, results);
+
+  return 0;
+}
